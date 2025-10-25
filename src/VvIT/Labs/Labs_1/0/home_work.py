8d6483ba812f8c87ce3a2b326608b0e02656197e
Stepan Zhnets <90891565+Stepan-Zhnets@users.noreply.github.com>
2025-10-25 14:05:42 +0300

    update

--- src/VvIT/Labs/Labs_1/0/home_work.py
+++ src/VvIT/Labs/Labs_1/0/home_work.py
@@ -1,16 +1,3 @@
-# Задача 1:
-
-print("Задача 1:")
-number = int(input("Введите число: "))
-for num in range(1, number+1):
-    print(num)
-
-print("Задача 2:")
-number_one = int(input("Введите первое число: "))
-number_two = int(input("Введите второе число: "))
-if number_one > number_two:
-    print(f"Большее число: {number_one}")
-elif number_one < number_two:
-    print(f"Большее число: {number_two}")
-else:
-    print("Числа равны!")
+num = int(input("Число: "))
+for x in range(1, num+1):
+    print(x)
---
+++ src/VvIT/Labs/Labs_1/0/hw.py
@@ -0,0 +1,9 @@
+print("Задача 2:")
+number_one = int(input("Введите первое число: "))
+number_two = int(input("Введите второе число: "))
+if number_one > number_two:
+    print(f"Большее число: {number_one}")
+elif number_one < number_two:
+    print(f"Большее число: {number_two}")
+else:
+    print("Числа равны!")
